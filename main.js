4b87f3b53eb4c87d3f5fbde8ad62c22431d37e59
teyotobias <teyotobias@utexas.edu>
2023-06-05 13:46:25 -0500

    base css and html setup

---
+++ main.js
@@ -0,0 +1,22 @@
+/*----- constants -----*/
+
+const COLORS = {
+    '0' : 'red',
+    '1' : 'yellow',
+    '2' : 'blue',
+    '3' : 'purple',
+    '4' : 'pink',
+    '5' : 'orange'
+}
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
