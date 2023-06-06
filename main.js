93168f4c5e1ac52990ca46d8e7d1958ffcb39c3c
teyotobias <teyotobias@utexas.edu>
2023-06-06 14:36:51 -0500

    Functionality done. Responsiveness and clean CSS needed

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ const COLORS = {
     '6' : 'orange'
 }
 const RESULTS = {
-    '0' : 'white',
-    '1' : 'black',
-    '2' : 'red'
+     0 : 'white',
+     1 : 'black',
+     2 : 'red'
 }
 
 
@@ -24,6 +24,7 @@ let winner; //null = no winner; 1 = winner
 let results; //array of 10 rows corresponding to color choices player makes
 let playerChoices;
 let colorCode;
+let playerResults;
 
 /*----- cached elements  -----*/
 const messageEl = document.getElementById('result-message');
@@ -32,11 +33,15 @@ const playAgainBtn = document.getElementById('play-again');
 
 
 /*----- event listeners -----*/
+document.getElementById('colors').addEventListener('click', handleChoice);
+document.getElementById('play-again').addEventListener('click', playAgain);
+document.getElementById('deselect').addEventListener('click', deselectAll);
+document.getElementById('select').addEventListener('click', handleSelect);
 
 
 /*----- functions -----*/
 init();
-
+//initalize board and data for game
 function init() {
     board = [
         [0,0,0,0],
@@ -51,19 +56,33 @@ function init() {
         [0,0,0,0],
     ];
     results = [
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
+        ['white', 'white', 'white', 'white'],
     ];
-    colorCode = [1,2,3,4]; //randomly generate, HCed just for testing
+    // results = [
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    //     [0,0,0,0],
+    // ];
+    //colorCode = ['1','2','3','4']; //randomly generate, HCed just for testing
+    colorCode = generateRandomColorCode();
     playerChoices = [0,0,0,0];
+    playerResults = [0,0,0,0];
     turn = 0;
     winner = null;
     colorCodeEl.style.visibility = 'hidden';
@@ -71,13 +90,64 @@ function init() {
     playAgainBtn.style.visibility = 'hidden';
     render();
 }
+
+//generate random color code for each game
+function generateRandomColorCode() {
+    let colorKeys = ['1','2','3','4','5','6'];
+    let randomColors = [];
+    for(let i = 0; i < 4; i++) {
+        let randIndex = Math.floor(Math.random() * colorKeys.length);
+        randomColors.push(colorKeys[randIndex]);
+        colorKeys.splice(randIndex, 1); // Remove the selected key to avoid repetition
+    }
+    return randomColors;
+
+}
+//calls determineOutcome after a turn has been finished
+//and prepares board / game logic for next turn
+function handleSelect() {
+    determineOutcome();
+    winner = getWinner() ? 1 : null;
+    //slice instead of = because if playerResults gets cleared after
+    // then so does results. slice creates a copy
+    results[turn] = playerResults.slice();
+    let count = playerResults.filter(val => val === 'red').length;
+    if(count == 0) {
+        messageEl.innerText = "You're Cold..."
+    }
+    else if(count == 1) {
+        messageEl.innerText = "Room Temp...";
+    }
+    else if(count == 2) {
+        messageEl.innerText = "Very Warm..."
+    }
+    else if(count == 3) {
+        messageEl.innerText = "You're Touching the Sun..."
+    }
+    playerChoices.fill(0);
+    playerResults.fill(0);
+    turn++;
+    render();
+    
+}
+//render function -> board, message, controls
 function render() {
     renderBoard();
     renderMessage();
-   // renderControls();
+    renderControls();
 }
+//deselects all colors selected for current turn
+function deselectAll() {
+    playerChoices.fill(0);
+    playerResults.fill(0);
+    board[turn].fill(0);
+    render();
 
+}
+//renders board layout
 function renderBoard() {
+    //remember to clear player results
+    //iterating over board and assigning each circle its color
     board.forEach(function(rowArr, rowIdx) {
         rowArr.forEach(function(cellVal, colIdx) {
             console.log(rowIdx, colIdx, cellVal);
@@ -88,14 +158,15 @@ function renderBoard() {
 
         });
     });
+    //iterating over result pegs and assigning each peg its color
     results.forEach(function(rowArr, rowIdx) {
         rowArr.forEach(function(cellVal, colIdx) {
             const cellId = `pegc${colIdx}r${rowIdx}`;
             const cellEl = document.getElementById(cellId);
-            cellEl.style.backgroundColor = COLORS[cellVal];
+            cellEl.style.backgroundColor = cellVal;
         });
 
-    })
+    });
     playerChoices.forEach(function(cellVal, choiceIdx) {
         const cellId = `choice${choiceIdx}`;
         const cellEl = document.getElementById(cellId);
@@ -108,16 +179,94 @@ function renderBoard() {
         cellEl.style.backgroundColor = COLORS[cellVal];
     });
 }
-
+//sends message to player based on win/loss
 function renderMessage() {
     if(turn === 10 || winner){
         messageEl.innerText = winner === null ? "You lose! Play again?":"You win! Play again?";
+        // colorCodeEl.style.visibility = 'visible';
+        //messageEl.style.visibility = 'visible';
+        // playAgainBtn.style.visibility = 'visible';
+    }
+    messageEl.style.visibility = 'visible';
+}
+//controls visibility of game outcome elements
+function renderControls() {
+    if(turn === 10 || winner) {
         colorCodeEl.style.visibility = 'visible';
         messageEl.style.visibility = 'visible';
         playAgainBtn.style.visibility = 'visible';
-
     }
 }
+//determines # of black, red pegs awarded to player
+function determineOutcome() {
 
-function renderControls() {
+    let playerChoicesCopy = [...playerChoices];
+    let colorCodeCopy = [...colorCode];
+    //first check if any match in right place - red pegs
+    for(let i = 0; i < playerChoices.length; i++) {
+        if(playerChoices[i] === colorCode[i]) {
+            playerResults[i] = RESULTS[2];  // Red peg
+            playerChoicesCopy[i] = null;
+            colorCodeCopy[i] = null;
+        }
+    }
+    //now check to see if any belong but are not in right place - black pegs
+    for(let i = 0; i < playerChoicesCopy.length; i++) {
+        if(playerChoicesCopy[i] !== null) {
+            let foundIndex = colorCodeCopy.indexOf(playerChoicesCopy[i]);
+            if(foundIndex !== -1) {
+                playerResults[i] = RESULTS[1];  // Black peg
+                colorCodeCopy[foundIndex] = null;
+            }
+        }
+    }
+    //playerChoices.fill(0);
+}
+function playAgain() {
+    init();
+}
+//checks to see if exists 4 red pegs -> they have guessed the code correctly
+function getWinner() {
+    return playerResults.every(val => val === RESULTS['2']); 
+}
+
+//handles a single selection of a color and updates board data
+//provides a guard for color selection as well
+function handleChoice(evt) {
+    console.log(evt.target);
+    //error checking right here
+    if(evt.target.id !== 'colors') {
+        const elID = evt.target.id;
+        const colorID = elID[5];
+        for(let i = 0; i < playerChoices.length; i++) {
+            if(playerChoices[i] == 0){
+                playerChoices[i] = colorID;
+                board[turn][i] = colorID;
+                break;
+            }
+        }  
+    }
+    // const elID = evt.target.id;
+    // const colorID = elID[5];
+    // for(let i = 0; i < playerChoices.length; i++) {
+    //     if(playerChoices[i] == 0){
+    //         playerChoices[i] = colorID;
+    //         board[turn][i] = colorID;
+    //         break;
+    //     }
+    // }
+    // if(playerChoices[3] !== 0) {
+    //     determineOutcome();
+    //     winner = getWinner() ? 1 : null;
+    //     //renderResults();
+    //     //playerChoices.fill(0);
+    //     //playerResults.fill(0);
+    //     //turn++;
+    //     results[turn] = playerResults.slice();
+    //     playerChoices.fill(0);
+    //     playerResults.fill(0);
+    //     turn++;
+    // }
+    //winner = getWinner();
+    render();
 }
